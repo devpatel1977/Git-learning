f7e9db8376f45f5b394329d050bcfbeb48ec797e
Dev Patel <devpatel25751977@gmail.com>
2025-03-08 18:34:38 +0530

    Payment updated after footer

--- first.js
+++ first.js
@@ -4,5 +4,10 @@ console.log(button)
 const login = "Login Page added";
 console.log(login);
 
+<<<<<<< HEAD
 const footer = "footer added in webside";
 console.log(footer);
+=======
+const  Payment = "Integrated the payment getway";
+console.log(Payment);
+>>>>>>> 65717bc (Payment added)
