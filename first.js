4a953a2763150746e88da00950c06076311bcf99
Dev Patel <devpatel25751977@gmail.com>
2025-03-08 21:05:44 +0530

    Dev commit

--- first.js
+++ first.js
@@ -4,16 +4,9 @@ console.log(button)
 const login = "Login Page added";
 console.log(login);
 
-<<<<<<< HEAD
 const footer = "footer added in webside";
 console.log(footer);
-=======
 const  Payment = "Integrated the payment getway";
 console.log(Payment);
-<<<<<<< HEAD
->>>>>>> 65717bc (Payment added)
-=======
 
-const upi = "Intergrated upi";
-console.log(upi);
->>>>>>> 84de2e3 (Upi added)
+console.log("latest update")
